f97bdba50a86439fcda45938fe826d2e51f860c3
GDavidDias <guillermo.david.dias@gmail.com>
2025-02-22 10:57:39 -0300

    First Commit para TEST PyR

---
+++ client/varGlobal.js
@@ -0,0 +1,9 @@
+const URL = 'http://localhost:3000';
+//const URL = 'https://cargos.salaprimariajpcd.net.ar';
+
+//const URL = 'https://cargos.gddhosting.net.ar';
+//const URL = 'https://cargosinicial.salaprimariajpcd.net.ar';
+//const URL = 'https://cargosupervisores.salaprimariajpcd.net.ar';
+
+
+export {URL}
